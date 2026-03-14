84c93d059305034a77992b438a0c2a500f1aba18
Ferdinando Primerano <f.primerano90@gmail.com>
2026-03-14 17:04:24 +0100

    feat: add Dockerfile and ability to serve application via Docker

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/api/ping', (req, res) => {
   res.json({ message: 'pong', status: 'server is running' });
